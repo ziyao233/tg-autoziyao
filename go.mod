13cc6d45d91a86a27c4f411121f26081d709eaa8
Yao Zi <ziyao@disroot.org>
2023-08-21 01:52:06 +0800

    Add an initial test

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/ziyao233/tg-autoziyao
+
+go 1.20
+
+replace github.com/ziyao233/trobot => ../trobot
+
+require github.com/ziyao233/trobot v0.0.0-00010101000000-000000000000
---
+++ main.go
@@ -0,0 +1,30 @@
+/*
+ *	tg-autoziyao
+ *	/main.go
+ *	By Mozilla Public License Version 2.0.
+ *	Copyright (c) 2023 Yao Zi. All rights reserved.
+ */
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ziyao233/trobot"
+       )
+
+func readBotToken() string {
+	token, err := os.ReadFile("token")
+	if err != nil {
+		panic("Cannot read from ./token")
+	}
+	// XXX: Remove \n: so this only works on systems using
+	// UNIX/Mac EOL
+	return string(token[:len(token) - 1])
+}
+
+func main() {
+	trobot.SetAPIToken(readBotToken())
+	fmt.Println(trobot.GetMe())
+}
